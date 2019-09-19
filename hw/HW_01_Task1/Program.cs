1d13d0bcd0ea2189c9f8cee8117409ee58140258
Vadim918 <vad.zav.91@mail.ru>
2019-09-19 21:10:13 +0300

    first prog

---
+++ hw/HW_01_Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HW_01_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
