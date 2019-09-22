29bcb5cdbe24a37d104b845673059519028dd1e8
Alexey Porotikov <alexey.porotikov@gmail.com>
2019-09-22 16:54:37 +0300

    hw 02 task 2

---
+++ hw_02/Task2/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = 1;
+            Int32 b1 = 2; 
+
+            Console.WriteLine("int a1 " + a1.GetType());
+            Console.WriteLine("Int32 b1 " + b1.GetType());
+            Console.WriteLine("__________________________________________");
+
+
+            Console.WriteLine("");
+            long a2 = 1;
+            Int64 b2 = 2;
+
+            Console.WriteLine("long a2 " + a2.GetType());
+            Console.WriteLine("Int64 b2 " + b2.GetType());
+            Console.WriteLine("__________________________________________");
+
+
+            Console.WriteLine("");
+            byte a3 = 1;
+            Byte b3 = 2;
+
+            Console.WriteLine("byte a3 " + a3.GetType());
+            Console.WriteLine("Byte b3 " + b3.GetType());
+            Console.WriteLine("__________________________________________");
+
+
+            Console.WriteLine("");
+            char a4 = '1';
+            Char b4 = '2';
+
+            Console.WriteLine("char a4 " + a4.GetType());
+            Console.WriteLine("Char b4 " + b4.GetType());
+            Console.WriteLine("__________________________________________");
+
+
+            Console.WriteLine("");
+            bool a5 = true;
+            Boolean b5 = false;
+
+            Console.WriteLine("bool a5 " + a5.GetType());
+            Console.WriteLine("Boolean b5 " + b5.GetType());
+            Console.WriteLine("__________________________________________");
+
+
+            Console.WriteLine("");
+            short a6 = 1;
+            Int16 b6 = 2;
+
+            Console.WriteLine("short a6 " + a6.GetType());
+            Console.WriteLine("Int16 b6 " + b6.GetType());
+            Console.WriteLine("__________________________________________");
+
+
+            Console.WriteLine("");
+            object a7 = 1;
+            Object b7 = 2;
+
+            Console.WriteLine("object a7 " + a7.GetType());
+            Console.WriteLine("Object b7 " + b7.GetType());
+            Console.WriteLine("__________________________________________");
+
+
+
+            Console.WriteLine("");
+            string a8 = "1";
+            String b8 = "2";
+
+            Console.WriteLine("string a8 " + a8.GetType());
+            Console.WriteLine("String b8 " + b8.GetType());
+            Console.WriteLine("__________________________________________");
+
+
+            Console.WriteLine("");
+            double a11 = 1;
+            Double b11 = 2;
+
+            Console.WriteLine("double a11 " + a11.GetType());
+            Console.WriteLine("Double b11 " + b11.GetType());
+            Console.WriteLine("__________________________________________");
+
+
+            Console.WriteLine("");
+            float a12 = 1;
+            Single b12 = 2;
+
+            Console.WriteLine("float a12 " + a12.GetType());
+            Console.WriteLine("Single b12 " + b12.GetType());
+            Console.WriteLine("__________________________________________");
+
+
+
+            Console.WriteLine("");
+            ushort a13 = 1;
+            UInt16 b13 = 2;
+
+            Console.WriteLine("ushort a13 " + a13.GetType());
+            Console.WriteLine("UInt16 b13 " + b13.GetType());
+            Console.WriteLine("__________________________________________");
+
+
+            Console.WriteLine("");
+            ulong a14 = 1;
+            UInt64 b14 = 2;
+
+            Console.WriteLine("ulong a14 " + a14.GetType());
+            Console.WriteLine("UInt64 b14 " + b14.GetType());
+            Console.WriteLine("__________________________________________");
+
+
+            Console.ReadKey();
+        }
+    }
+}
