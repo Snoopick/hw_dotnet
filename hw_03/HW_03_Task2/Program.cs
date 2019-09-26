a4c5b59492e6bc8e57a23a5137a484c7acb3dfb2
Alexey Porotikov <alexey.porotikov@gmail.com>
2019-09-26 19:54:37 +0300

    homework 3

---
+++ hw_03/HW_03_Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HW_03_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first num:");
+            string num1 = Console.ReadLine();
+
+            Console.WriteLine("Enter second num:");
+            string num2 = Console.ReadLine();
+
+            Console.WriteLine("Enter sum as you think:");
+            string sum = Console.ReadLine();
+
+            int rightSum = sumFunction(Convert.ToInt32(num1), Convert.ToInt32(num2));
+
+            if (Convert.ToInt32(sum) == rightSum)
+            {
+                Console.WriteLine("You right");
+            }
+            else
+            {
+                Console.WriteLine("You are fault");
+            }
+
+            
+
+            Console.ReadKey();
+        }
+
+        static int sumFunction(int num1 = 0, int num2 = 0)
+        {
+            return (num1 + num2);
+        }
+    }
+}
