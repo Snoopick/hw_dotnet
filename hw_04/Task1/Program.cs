dc59491563b97237c0b825d4207ff1d67a2717f2
Alexey Porotikov <alexey.porotikov@gmail.com>
2019-09-30 20:25:51 +0300

    homework 4

---
+++ hw_04/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alphavit = "abcdefghijklmnopqrstuvwxyz";
+
+            for (int i = 25; i >= 0; i--)
+            {
+                Console.WriteLine(alphavit[i]);
+            }
+        }
+    }
+}
---
+++ hw_04/Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startSum = 1000;
+            int monthCount = 12;
+            int resultSum = startSum;
+            int monthSum = 0;
+
+            for (int i = 1; i <= monthCount; i++)
+            {
+                monthSum = resultSum * 2 / 100;
+
+                if (i < 11)
+                {
+                    Console.WriteLine("At " + i + " month sum will be more at " + monthSum);
+                }
+
+                resultSum += monthSum;
+
+
+                if (i > 3)
+                {
+                    Console.WriteLine("The sum on " + i + " month will be at " + resultSum);
+                }
+            }
+        }
+    }
+}
