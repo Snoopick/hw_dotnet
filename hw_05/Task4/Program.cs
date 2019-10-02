d22c2c2ab1df6e7e99349f54e5f65ba04a325fe4
Alexey Porotikov <alexey.porotikov@gmail.com>
2019-10-02 22:38:28 +0300

    homework 5

---
+++ hw_05/Task4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Task4 {
+    class Program {
+        static void Main(string[] args) {
+            int arraySize = 100000;
+            long[] originalArray = new long[arraySize];
+            long[] array = new long[arraySize];
+            long index1 = 0;
+            long index2 = 0;
+            Stopwatch stopwatch = new Stopwatch();
+            Stopwatch stopwatch2 = new Stopwatch();
+            Random rand = new Random();
+
+            for (int i = 0; i < arraySize; i++) {
+                array[i] = rand.Next(1, 100);
+            }
+
+            originalArray = array;
+
+            stopwatch2.Start();
+            Array.Reverse(originalArray);
+            stopwatch2.Stop();
+
+            Console.WriteLine("Standart reverse work " + stopwatch2.Elapsed);
+
+            stopwatch.Start();
+
+            for (int i = 0; i < arraySize; i++) {
+                int j = arraySize - 1 - i;
+
+                if (i >= j) {
+                    break;
+                }
+
+                index1 = array[i];
+                index2 = array[j];
+
+                array[i] = index2;
+                array[j] = index1;
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine("My reverse work " + stopwatch.Elapsed);
+        }
+    }
+}
---
+++ hw_05/hw_05/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace hw_05 {
+    class Program {
+        static void Main(string[] args) {
+            int arraySize = 10;
+            int[] firstArray = new int[arraySize];
+            int[] secondArray = new int[arraySize];
+            int[] rerultArray = new int[arraySize];
+            Random rand = new Random();
+
+            for (int i = 0; i < arraySize; i++) {
+                firstArray[i] = rand.Next(1, 100);
+            }
+
+            for (int i = 0; i < arraySize; i++) {
+                Console.WriteLine("Enter num:");
+                secondArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < arraySize; i++) {
+                rerultArray[i] = firstArray[i] + secondArray[i];
+            }
+
+            Console.WriteLine("First array:");
+            Console.WriteLine(string.Join(", ", firstArray));
+
+            Console.WriteLine("Second array:");
+            Console.WriteLine(string.Join(", ", secondArray));
+
+            Console.WriteLine("Result array:");
+            Console.WriteLine(string.Join(", ", rerultArray));
+        }
+    }
+}
