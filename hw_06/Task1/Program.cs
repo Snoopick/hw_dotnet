2e58feb347423bd86aea52b2c36e5abe67b4a35e
Alexey Porotikov <alexey.porotikov@gmail.com>
2019-10-05 18:06:01 +0300

    homework 6

---
+++ hw_06/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task1 {
+    class Program {
+        static void Main(string[] args) {
+            string theLine = "";
+
+            Console.WriteLine("Write the poem, seporate line \";\" char");
+            theLine = Console.ReadLine();
+
+            String[] stringArray = theLine.Split(";");
+
+            for (int i = 0; i < stringArray.Length; i++) {
+                stringArray[i] = stringArray[i].Replace("O", "A");
+            }
+
+            Console.WriteLine(string.Join("\n\n" + new String('-', 10) + "\n\n", stringArray));
+        }
+    }
+}
