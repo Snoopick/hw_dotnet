778007342110d9758ec25fcdee85ce4f5de40f7e
Alexey Porotikov <alexey.porotikov@gmail.com>
2019-10-20 19:26:16 +0300

    hw_09

---
+++ hw_09/hw_09/Motorcycle.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hw_09 {
+    class Motorcycle {
+        private Dictionary<string, string> data = new Dictionary<string, string> { };
+        public Motorcycle() {
+            this.data.Add("VIN", "VIN-CODE-13412-ASDAS");
+            this.data.Add("WEIGHT", "200");
+            this.data.Add("ENGINE", "2000");
+
+            Console.WriteLine("Get data from array:");
+            this.GetData();
+
+            Console.WriteLine("Save data");
+            this.SaveData(this.data);
+
+            Console.WriteLine("Update WEIGHT to 2200");
+            this.UpdateData("WEIGHT", "2200");
+
+            Console.WriteLine("Get data by key WEIGHT");
+            this.GetData("WEIGHT");
+
+            Console.WriteLine("Delete data by key ENGINE");
+            this.DeleteData("ENGINE");
+
+            Console.WriteLine("Get result data");
+            this.GetData();
+        }
+
+        void SaveData(Dictionary<string, string> data) {
+            MyLogger.Log(2, "Save data action");
+            if (data.Equals(string.Empty)) {
+                MyLogger.Log(1, "Empty data");
+            }
+
+            if (!CRUD.Save(data)) {
+                MyLogger.Log(1, "Data not Saved");
+            }
+        }
+
+        void GetData(string key = "") {
+            MyLogger.Log(2, "Get data action");
+
+
+            if (key != string.Empty) {
+                MyLogger.Log(2, "Get data by ID action");
+                string resultArray = CRUD.GetByID(key);
+                Console.WriteLine("Key: " + key + " data: " + resultArray);
+            } else {
+                var resultArray = CRUD.GetAll();
+                foreach (var value in resultArray) {
+                    Console.WriteLine(value);
+                }
+            }
+        }
+
+        void UpdateData(string key, string data) {
+            MyLogger.Log(2, "Update data action");
+
+            if (key.Equals(string.Empty)) {
+                MyLogger.Log(1, "Key is empty");
+            }
+
+            if (!CRUD.Update(key, data)) {
+                MyLogger.Log(1, "Data not updated");
+            }
+        }
+
+        void DeleteData(string key) {
+            MyLogger.Log(2, "Delete data action");
+
+            if (key.Equals(string.Empty)) {
+                MyLogger.Log(1, "Key is empty");
+            }
+
+            if (!CRUD.Delete(key)) {
+                MyLogger.Log(1, "Data not deleted");
+            }
+        }
+    }
+}
---
+++ hw_09/hw_09/MyLogger.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace hw_09 {
+    public static class MyLogger {
+        private const string LoggerLogPath = @"..\\..\\..\\logs\\";
+        private static string lastError = string.Empty;
+        private static int logType = 0;
+        private static string defaultFileName = string.Empty;
+        private static string defaultFilePath = string.Empty;
+
+        public static string Log(int type = 0, string str = "") {
+            string filePath = string.Empty;
+            string date = DateTime.Now.ToString("dd.MM.yyy");
+
+            if (type == 0) {
+                MyLogger.Log(1, "No logger type");
+                return MyLogger.lastError;
+            }
+
+            MyLogger.logType = type;
+
+            if (str.Equals(string.Empty)) {
+                MyLogger.Log(1, "Empty logger string");
+                return MyLogger.lastError;
+            }
+
+            str = date + " - " + str;
+
+            switch (type) {
+                case 1:
+                    MyLogger.lastError = str;
+                    str += "\r\nBug trace\r\n";
+                    break;
+                case 2:
+                    break;
+                default:
+                    MyLogger.Log(1, "Unknown type for log");
+                    return MyLogger.lastError;
+            }
+
+            str += "\r\n";
+
+            filePath = MyLogger.getFile();
+
+            MyLogger.LogWriter(filePath, str);
+            MyLogger.LogWriter(defaultFilePath, str);
+
+            return string.Empty;
+        }
+
+        private static string getFile() {
+            string date = DateTime.Now.ToString("dd.MM.yyy");
+            MyLogger.defaultFileName = date + "_loggerLog";
+            MyLogger.defaultFilePath = LoggerLogPath + defaultFileName;
+            string fileName = string.Empty;
+
+            fileName = MyLogger.PrepareFolder();
+
+            return fileName;
+        }
+
+        private static string PrepareFolder(int newIndex = 0, bool needCreate = false) {
+            if (needCreate) {
+                File.Create(LoggerLogPath + defaultFileName + "_" + newIndex + ".txt");
+            }
+
+            List<string> files = Directory
+                .GetFiles(LoggerLogPath, "*", SearchOption.AllDirectories)
+                .ToList();
+            int fileCount = files.Count();
+            int fileIndex = 0;
+
+            if (fileCount > 0) {
+                fileIndex = fileCount - 1;
+            }
+
+            string filePath = LoggerLogPath + defaultFileName + "_" + fileIndex + ".txt";
+
+            if (!File.Exists(filePath)) {
+                File.Create(filePath);
+                files.Add(defaultFileName + "_" + fileIndex + ".txt");
+            }
+
+            long fileSize = new FileInfo(filePath).Length;
+
+            if (fileSize > (50 * 1024)) {
+                PrepareFolder((fileIndex + 1), true);
+            }
+
+            return filePath;
+        }
+
+        private static void LogWriter(string filePath, string str) {
+            FileInfo fileInf = new FileInfo(filePath);
+
+            if (fileInf.Exists) {
+                try {
+                    using (StreamWriter sw = new StreamWriter(filePath, true, System.Text.Encoding.Default)) {
+                        sw.WriteLine(str);
+                    }
+                } catch (Exception e) {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
